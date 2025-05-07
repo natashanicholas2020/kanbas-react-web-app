01d5d06e88c5f682bef4eb3902c5b8983385ebaa
Natasha Nicholas <nnicholas2020@gmail.com>
2025-05-07 13:50:48 -0400

    up to 1.4.6

---
+++ src/Kambaz/Courses/index.tsx
@@ -0,0 +1,28 @@
+import CourseNavigation from "./Navigation";
+import Modules from "./Modules";
+import { Navigate, Route, Routes } from "react-router";
+export default function Courses() {
+    return (
+      <div id="wd-courses">
+        <h2>Course 1234</h2>
+        <hr />
+      <table>
+        <tr>
+          <td valign="top">
+            <CourseNavigation />
+          </td>
+          <td valign="top">
+            <Routes>
+              <Route path="/" element={<Navigate to="Home" />} />
+              <Route path="Home" element={<h2>Home</h2>} />
+              <Route path="Modules" element={<Modules />} />
+              <Route path="Assignments" element={<h2>Assignments</h2>} />
+              <Route path="Assignments/:aid" element={<h2>Assignment Editor</h2>} />
+              <Route path="People" element={<h2>People</h2>} />
+            </Routes>
+          </td>
+        </tr>
+      </table>
+      </div>
+  );}
+  
