4b1dd27d95345de02c251ed571fa01dcd53a8668
Natasha Nicholas <nnicholas2020@gmail.com>
2025-05-07 13:20:18 -0400

    completed 1.4.2

--- src/App.tsx
+++ src/App.tsx
@@ -35,15 +35,21 @@
 // export default App
 
 import Labs from "./Labs";
-import { HashRouter } from "react-router-dom";
+import Kambaz from "./Kambaz";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 export default function App() {
   return (
     <HashRouter>
       <div>
-        <Labs />
+      <Routes>
+      <Route path="/" element={<Navigate to="Kambaz"/>}/>
+     <Route path="/Labs/*" element={<Labs />} />
+     <Route path="/Kambaz/*" element={<Kambaz />} />
+        </Routes>
       </div>
     </HashRouter>
 );}
+
 
 
 // The App component is the root component that imports other components
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,11 +1,23 @@
 import Lab1 from "./Lab1";
+import { Route, Routes, Navigate } from "react-router";
+import TOC from "./TOC";
+import Lab2 from "./Lab2";
+import Lab3 from "./Lab3";
 export default function Labs() {
   return (
-    <div id="wd-labs">
+    <div>
       <h1>Labs</h1>
-      <Lab1 />
+      <TOC />
+      <Routes>
+        <Route path="/" element={<Navigate to="Lab1" />} />
+        <Route path="Lab1" element={<Lab1 />} />
+        <Route path="Lab2/*" element={<Lab2 />} />
+        <Route path="Lab3/*" element={<Lab3 />} />
+      </Routes>
     </div>
 );}
+
+
 // Components can import other components to aggregate the code snippets
 // of the components into larger, more complex HTML content. Here the
 // Labs component imports the Lab1 component as the first of a
