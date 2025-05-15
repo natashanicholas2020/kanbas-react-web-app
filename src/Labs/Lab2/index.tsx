b147c973e0afb10f7e58e7df7125f16e0f521b93
Natasha Nicholas <nnicholas2020@gmail.com>
2025-05-15 17:02:47 -0400

    Made some changes

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,4 +1,8 @@
 import "./index.css";
+import BackgroundColors from "./BackgroundColors";
+import Borders from "./Borders";
+import Padding from "./Padding";
+import Margins from "./Margins";
 export default function Lab2() {
   return (
     <div id="wd-lab2">
@@ -57,6 +61,20 @@ This heading has same style as paragraph above
     </div>
   </div>
 </div>
+
+<div id="wd-css-colors">
+  <h2>Colors</h2>
+  <h3 className="wd-fg-color-blue">Foreground color</h3>
+  <p className="wd-fg-color-red">
+    The text in this paragraph is red but
+    <span className="wd-fg-color-green"> this text is green</span>
+  </p>
+</div>
+
+<BackgroundColors />
+<Borders />
+<Padding />
+<Margins />
 
 
 
