4f60ae32d1c2dbf59122a8ef97d44c0f72a3eebb
Natasha Nicholas <nnicholas2020@gmail.com>
2025-05-20 16:10:18 -0400

    completed 3.2.4

--- src/Labs/Lab3/BooleanVariables.tsx
+++ src/Labs/Lab3/BooleanVariables.tsx
@@ -1,4 +1,4 @@
-export default function VariablesAndConstants() {
+export default function BooleanVariables() {
     let numberVariable = 123, floatingPointNumber = 234.345;
     let true1 = true, false1 = false;
     let false2 = true1 && false1;
--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,6 +1,7 @@
 import VariablesAndConstants from "./VariablesandConstants";
 import VariableTypes from "./VariableTypes";
 import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
 export default function Lab3() {
   return (
     <div id="wd-lab3">
@@ -8,6 +9,7 @@ export default function Lab3() {
       <VariablesAndConstants/>
       <VariableTypes/>
       <BooleanVariables/>
+      <IfElse/>
     </div>
   );
 }
