4b215853b44b776cd512767c44e4e40a85949580
Natasha Nicholas <nnicholas2020@gmail.com>
2025-05-21 11:09:20 -0400

    finished 3.4

---
+++ src/Labs/Lab3/DestructingImports.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Math, { add, subtract, multiply, divide } from "./Math";
+import * as Matematica from "./Math";
+export default function DestructingImports() {
+  return (
+    <div id="wd-destructuring-imports">
+      <h2>Destructing Imports</h2>
+      <table className="table table-sm">
+        <thead>
+          <tr>
+            <th>Math</th>
+            <th>Matematica</th>
+            <th>Functions</th>
+          </tr>
+        </thead>
+        <tbody>
+        <tr>
+  <td>Math.add(2, 3) = {Math.add(2, 3)}</td>
+  <td>Matematica.add(2, 3) =
+     {Matematica.add(2, 3)}</td>
+  <td>add(2, 3) = {add(2, 3)}</td>
+</tr>
+<tr>
+  <td>Math.subtract(5, 1) = {Math.subtract(5, 1)}</td>
+  <td>Matematica.subtract(5, 1) =
+     {Matematica.subtract(5, 1)}</td>
+  <td>subtract(5, 1) = {subtract(5, 1)}</td>
+</tr>
+<tr>
+  <td>Math.multiply(5, 6) = {Math.multiply(5, 6)}</td>
+  <td>Matematica.multiply(5, 6) =
+     {Matematica.multiply(5, 6)}</td>
+  <td>multiply(5, 6) = {multiply(5, 6)}</td>
+</tr>
+<tr>
+  <td>Math.divide(14, 2) = {Math.divide(14, 2)}</td>
+  <td>Matematica.divide(14, 2) =
+     {Matematica.divide(14, 2)}</td>
+  <td>divide(14, 2) = {divide(14, 2)}</td>
+</tr>
+
+        </tbody>
+      </table>
+      <hr />
+    </div>
+);}
--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -5,6 +5,25 @@ import IfElse from "./IfElse";
 import TernaryOperator from "./TernaryOperator";
 import ConditionalOutputIfElse from "./ConditionalOutputIfElse"
 import ConditionalOutputInline from "./ConditionalOutputInline"
+import LegacyFunctions from "./LegacyFunctions"
+import ArrowFunctions from "./ArrowFunctions"
+import ImpliedReturn from "./ImpliedReturn"
+import TemplateLiterals from "./TemplateLiterals"
+import SimpleArrays from "./SimpleArrays"
+import ArrayIndexAndLength from "./ArrayIndexAndLength"
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays"
+import ForLoops from "./ForLoops"
+import MapFunction from "./MapFunction"
+import FindFunction from "./FindFunction"
+import FindIndex from "./FindIndex"
+import FilterFunction from "./FilterFunction"
+import JsonStringify from "./JsonStringify"
+import House from "./House"
+import TodoList from "./todo/TodoList"
+import Spreading from "./Spreading"
+import Destructing from "./Destructing"
+import FunctionDestructing from "./FunctionDestructing"
+import DestructingImports from "./DestructingImports"
 export default function Lab3() {
   return (
     <div id="wd-lab3">
@@ -16,6 +35,25 @@ export default function Lab3() {
       <TernaryOperator/>
       <ConditionalOutputIfElse/>
       <ConditionalOutputInline/>
+      <LegacyFunctions/>
+      <ArrowFunctions/>
+      <ImpliedReturn/>
+      <TemplateLiterals/>
+      <SimpleArrays/>
+      <ArrayIndexAndLength/>
+      <AddingAndRemovingToFromArrays/>
+      <ForLoops/>
+      <MapFunction/>
+      <FindFunction/>
+      <FindIndex/>
+      <FilterFunction/>
+      <JsonStringify/>
+      <House/>
+      <TodoList/>
+      <Spreading/>
+      <Destructing/>
+      <FunctionDestructing/>
+      <DestructingImports/>
     </div>
   );
 }
