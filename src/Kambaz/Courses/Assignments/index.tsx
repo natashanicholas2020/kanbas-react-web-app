c5e11db60197b4229fae3cdab66a6ea4627b289d
Natasha Nicholas <nnicholas2020@gmail.com>
2025-05-27 11:42:41 -0400

    halfway through 3.8.8

--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -8,7 +8,6 @@ export default function Assignments() {
   const assignments = db.assignments;
   return (
     <div id="wd-assignments" className="p-3">
-      
       <div className="d-flex align-items-center mb-3">
         <Form.Control
           placeholder="Search for Assignments"
@@ -41,6 +40,11 @@ export default function Assignments() {
         </Button>
       </h3>
 
+      {assignments
+          .filter((assignments: any) => assignments.course === cid)
+          .map((assignments: any) => (
+            
+
       <ul id="wd-assignment-list" className="list-unstyled">
         <li className="wd-assignment-list-item d-flex align-items-center mb-3">
           <div className="border-start border-3 border-success ps-3 pe-3">
@@ -48,7 +52,7 @@ export default function Assignments() {
               href="#/Kambaz/Courses/1234/Assignments/123"
               className="wd-assignment-link fs-5 text-decoration-none text-dark"
             >
-              A1 - ENV + HTML
+              {assignments._id && assignments.title}
             </a>
             <div className="wd-assignment-meta text-muted">
               Multiple Modules | <b>Not available until</b> May 6 at 12:00 AM |
@@ -58,8 +62,9 @@ export default function Assignments() {
             </div>
           </div>
         </li>
+        ))}
 
-        <li className="wd-assignment-list-item d-flex align-items-center mb-3">
+        {/* <li className="wd-assignment-list-item d-flex align-items-center mb-3">
           <div className="border-start border-3 border-success ps-3 pe-3">
             <a
               href="#/Kambaz/Courses/1234/Assignments/123"
@@ -91,8 +96,8 @@ export default function Assignments() {
               <b>Due</b> May 27 at 11:59 PM | 100 pts
             </div>
           </div>
-        </li>
+        </li> */}
       </ul>
-    </div>
+  //</div>
   );
 }
