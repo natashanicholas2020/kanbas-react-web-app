deae4d24e4f0ea1d1b8f6ccaa0c57fc29cc31d5d
Natasha Nicholas <nnicholas2020@gmail.com>
2025-05-27 21:45:42 -0400

    Revert "completed 3.6"
    This reverts commit ab31a6731ccdd53276f54fbf714c34510c7458d2.

--- src/Labs/Lab3/Square.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import type { ReactNode } from "react";
-
-export default function Square({ children }: { children: ReactNode }) {
-  const num = Number(children);
-  return <span id="wd-square">{num * num}</span>;
-}
--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -24,11 +24,6 @@ import Spreading from "./Spreading"
 import Destructing from "./Destructing"
 import FunctionDestructing from "./FunctionDestructing"
 import DestructingImports from "./DestructingImports"
-import Classes from "./Classes"
-import Add from "./Add";
-import Square from "./Square";
-import Highlight from "./Highlight";
-import PathParameters from "./PathParameters";
 export default function Lab3() {
   console.log('Hello World!');
   return (
@@ -60,16 +55,6 @@ export default function Lab3() {
       <Destructing/>
       <FunctionDestructing/>
       <DestructingImports/>
-      <Classes/>
-      <Add a={3} b={4} />
-      <h4>Square of 4</h4>
-      <Square>4</Square>
-      <hr />
-      <Highlight>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
-        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
-     </Highlight>
-     <PathParameters/>
     </div>
   );
 }
