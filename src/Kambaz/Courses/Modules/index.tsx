77477e865f294f5c0e4ce843690315c96deb22a5
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-02 22:00:57 -0400

    finished 4.4.3

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -1,37 +1,84 @@
+import React, { useState } from "react";
 import { useParams } from "react-router";
-import * as db from "../../Database";
-import { ListGroup } from "react-bootstrap";
+import { ListGroup, FormControl } from "react-bootstrap";
 import ModulesControls from "./ModulesControls";
-import LessonControlButtons from "./LessonControlButtons";
-import ModuleControl from "./ModulesControls";
+import ModuleControlButtons from "./ModuleControlButtons";
 import { BsGripVertical } from "react-icons/bs";
+import { useSelector, useDispatch } from "react-redux";
+import { addModule, editModule, updateModule, deleteModule } from "./reducer";
 
 export default function Modules() {
   const { cid } = useParams();
-  const modules = db.modules;
-  return (
-    <div>
-      <ModulesControls />
-      <br /><br /><br /><br />
+  const [moduleName, setModuleName] = useState("");
+  const { modules } = useSelector((state: any) => state.modulesReducer);
+  const dispatch = useDispatch();
 
-      <ListGroup className="rounded-0" id="wd-modules">
-      {modules
+  if (!cid) return <div>No course selected.</div>;
+
+  return (
+    <div className="wd-modules">
+      <ModulesControls
+        moduleName={moduleName}
+        setModuleName={setModuleName}
+        addModule={() => {
+          dispatch(addModule({ name: moduleName, course: cid }));
+          setModuleName("");
+        }}
+      />
+
+      <ListGroup id="wd-modules" className="rounded-0">
+        {modules
           .filter((module: any) => module.course === cid)
           .map((module: any) => (
-        <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary">
-            <BsGripVertical className="me-2 fs-3" />
-            {module.name} <ModuleControl />
-          </div>
-          {module.lessons && (
-          <ListGroup className="wd-lessons rounded-0">
-            {module.lessons.map((lesson: any) => (
-            <ListGroup.Item className="wd-lesson p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" />
-              {lesson.name} <LessonControlButtons />
+            <ListGroup.Item
+              key={module._id}
+              className="wd-module p-0 mb-5 fs-5 border-gray"
+            >
+              <div className="wd-title p-3 ps-2 bg-secondary d-flex align-items-center justify-content-between">
+                <div className="d-flex align-items-center">
+                  <BsGripVertical className="me-2 fs-3" />
+                  {!module.editing && module.name}
+                  {module.editing && (
+                    <FormControl
+                      className="w-50 d-inline-block"
+                      defaultValue={module.name}
+                      onChange={(e) =>
+                        dispatch(updateModule({ ...module, name: e.target.value }))
+                      }
+                      onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                          dispatch(updateModule({ ...module, editing: false }));
+                        }
+                      }}
+                      onBlur={() => dispatch(updateModule({ ...module, editing: false }))}
+                      autoFocus
+                    />
+                  )}
+                </div>
+
+                <ModuleControlButtons
+                  moduleId={module._id}
+                  deleteModule={(moduleId) => dispatch(deleteModule(moduleId))}
+                  editModule={(moduleId) => dispatch(editModule(moduleId))}
+                />
+              </div>
+
+              {module.lessons && (
+                <ListGroup className="wd-lessons rounded-0">
+                  {module.lessons.map((lesson: any) => (
+                    <ListGroup.Item
+                      key={lesson._id}
+                      className="wd-lesson p-3 ps-1"
+                    >
+                      {lesson.name}
+                      {/* Add LessonControlButtons if needed */}
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
             </ListGroup.Item>
-            ))}
-            </ListGroup>)}</ListGroup.Item>))}</ListGroup>
+          ))}
+      </ListGroup>
     </div>
   );
 }
