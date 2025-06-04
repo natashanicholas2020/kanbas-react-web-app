ab6d2f950f74f07741c6efcbfd64a6e33a5e2f84
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-03 21:35:55 -0400

    minor edits

--- src/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
+++ src/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
@@ -1,16 +1,13 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { deleteTodo, setTodo } from "./todosReducer";
-import { ListGroup, Button} from "react-bootstrap";
-export default function TodoItem({ todo,
- }) {
-  const dispatch = useDispatch();
-  return (
-    <ListGroup.Item key={todo.id}>
-      <Button onClick={() => dispatch(deleteTodo(todo.id))}
-              id="wd-delete-todo-click"> Delete </Button>
-      <Button onClick={() => dispatch(setTodo(todo))}
-              id="wd-set-todo-click"> Edit </Button>
-      {todo.title}
-    </ListGroup.Item>
-);}
+import { ListGroup, Button } from "react-bootstrap";
+export default function TodoItem({ todo, deleteTodo, setTodo }: {
+    todo: { id: string; title: string };
+    deleteTodo: (id: string) => void;
+    setTodo: (todo: { id: string; title: string }) => void;
+  }) {
+    return (
+      <ListGroup.Item key={todo.id}>
+        <Button onClick={() => deleteTodo(todo.id)}
+                id="wd-delete-todo-click"> Delete </Button>
+        <Button onClick={() => setTodo(todo)}
+                id="wd-set-todo-click"> Edit </Button>
+        {todo.title}    </ListGroup.Item>);}
