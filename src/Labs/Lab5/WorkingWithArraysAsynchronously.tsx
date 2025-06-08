b6a37dec646f569061673b1d4899f3d4c39ef1bb
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-08 19:02:28 -0400

    finished 5.2.5

---
+++ src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { ListGroup } from "react-bootstrap";
+import { FaTrash } from "react-icons/fa";
+import { FaPlusCircle } from "react-icons/fa";
+
+import * as client from "./client";
+export default function WorkingWithArraysAsynchronously() {
+  const [todos, setTodos] = useState<any[]>([]);
+  const createTodo = async () => {
+    const todos = await client.createTodo();
+    setTodos(todos);
+  };
+
+  const fetchTodos = async () => {
+    const todos = await client.fetchTodos();
+    setTodos(todos);
+  };
+  const removeTodo = async (todo: any) => {
+    const updatedTodos = await client.removeTodo(todo);
+    setTodos(updatedTodos);
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+  return (
+    <div id="wd-asynchronous-arrays">
+      <h3>Working with Arrays Asynchronously</h3>
+      <h4>Todos 
+      <FaPlusCircle onClick={createTodo} className="text-success float-end fs-3"
+                         id="wd-create-todo" />
+      </h4>
+      <ListGroup>
+        {todos.map((todo) => (
+          <ListGroup.Item key={todo.id}>
+            <FaTrash onClick={() => removeTodo(todo)}
+                     className="text-danger float-end mt-1" id="wd-remove-todo"/>
+            <input type="checkbox" className="form-check-input me-2"
+                   defaultChecked={todo.completed}/>
+            <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
+              {todo.title}
+            </span>
+          </ListGroup.Item>
+        ))}
+      </ListGroup> <hr />
+    </div>
+);}
