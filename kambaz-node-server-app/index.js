2251b584037aa7ff4471236108b79375b0b6e185
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-09 14:56:26 -0400

    need to do 5.2.4.7

--- kambaz-node-server-app/index.js
+++ kambaz-node-server-app/index.js
@@ -1,10 +1,7 @@
 import express from 'express'
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
-import cors from "cors";
 const app = express()
-app.use(cors());   
-app.use(express.json());
-Lab5(app)
+Lab5(app);
 Hello(app)
 app.listen(process.env.PORT || 4000)
