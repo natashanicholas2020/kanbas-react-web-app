4657bf83bd6da545578f116ac54301bb61aed472
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-10 23:38:52 -0400

    fixed

--- src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
+++ src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
@@ -9,7 +9,7 @@ import { FormControl } from "react-bootstrap";
 
 export default function WorkingWithArraysAsynchronously() {
   const [todos, setTodos] = useState<any[]>([]);
-  //const [errorMessage, setErrorMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const editTodo = (todo: any) => {
     const updatedTodos = todos.map(
@@ -18,12 +18,12 @@ export default function WorkingWithArraysAsynchronously() {
   };
 
   const updateTodo = async (todo: any) => {
-    //try {
+    try {
       await client.updateTodo(todo);
       setTodos(todos.map((t) => (t.id === todo.id ? todo : t)));
-   // } catch (error: any) {
-      //setErrorMessage(error.response.data.message);
-    //}
+    } catch (error: any) {
+      setErrorMessage(error.response.data.message);
+    }
   };
 
   const createTodo = async () => {
@@ -47,15 +47,14 @@ export default function WorkingWithArraysAsynchronously() {
   };
 
   const deleteTodo = async (todo: any) => {
-    //try {
+    try {
       await client.deleteTodo(todo);
       const newTodos = todos.filter((t) => t.id !== todo.id);
       setTodos(newTodos);
-    //} catch (error: any) {
-    //  console.log(error);
-    //  setErrorMessage(error.response.data.message);
-    //}  
-  };
+    } catch (error: any) {
+      console.log(error);
+      setErrorMessage(error.response.data.message);
+    }  };
 
   useEffect(() => {
     fetchTodos();
@@ -64,7 +63,7 @@ export default function WorkingWithArraysAsynchronously() {
   return (
     <div id="wd-asynchronous-arrays">
       <h3>Working with Arrays Asynchronously</h3>
-      {/* {errorMessage && (<div id="wd-todo-error-message" className="alert alert-danger mb-2 mt-2">{errorMessage}</div>)} */}
+      {errorMessage && (<div id="wd-todo-error-message" className="alert alert-danger mb-2 mt-2">{errorMessage}</div>)}
       <h4>Todos
       <FaPlusCircle onClick={createTodo} className="text-success float-end fs-3"
                          id="wd-create-todo" />
