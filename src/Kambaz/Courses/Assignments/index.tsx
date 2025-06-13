7c834d6b155510ea7fce6b751fa29f9fff9bdf8e
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-13 16:12:59 -0400

    completed 3.8.8

--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -4,62 +4,44 @@ import ModuleControlButtons from "./AssignmentControlButtons";
 import LessonControlButtons from "./LessonControlButtons";
 import { ListGroup } from "react-bootstrap";
 import { BsGripVertical } from "react-icons/bs";
+import { useParams } from "react-router";
+import * as db from "../../Database";
 
 export default function Assignments() {
+    const { cid } = useParams();
+  
+    // const assignments = db.assignments;
+
+    const assignments = db.assignments.filter((a) => a.course === cid);
+
   return (
     <div>
-      <ModulesControls /><br /><br /><br />
+      <ModulesControls />
+      <br /><br /><br />
       <ListGroup className="rounded-0" id="wd-assignments">
-        <ListGroup.Item className="wd-assignment p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary text-white d-flex justify-content-between align-items-center">
-            <div>
-              <BsGripVertical className="me-2 fs-3" />
-              Assignments
+        {assignments.map((assignment) => (
+          <ListGroup.Item key={assignment._id} className="wd-assignment p-0 mb-5 fs-5 border-gray">
+            <div className="wd-title p-3 ps-2 bg-secondary text-white d-flex justify-content-between align-items-center">
+              <div>
+                <BsGripVertical className="me-2 fs-3" />
+                {assignment.title}
+              </div>
+              <ModuleControlButtons />
             </div>
-            <ModuleControlButtons />
-          </div>
 
-          <ListGroup className="wd-assignments rounded-0">
-            <ListGroup.Item className="wd-lesson p-3 ps-1 d-flex justify-content-between align-items-center">
-              <div>
-                <BsGripVertical className="me-2 fs-3" />
-                    <a href="#/Kambaz/Courses/1234/Assignments/123"
-                className="wd-assignment-link" >
-                A1 - ENV + HTML
-                </a> 
-                    <div> Multiple Modules | <b>Not available until</b> May 6 at 12:00am | </div>
-                    <div> <b>Due</b> May 13 at 11:59pm | 100 pts </div>
+            <ListGroup className="wd-lessons rounded-0">
+              <ListGroup.Item className="wd-lesson p-3 ps-1 d-flex justify-content-between align-items-center">
+                <div>
+                  <BsGripVertical className="me-2 fs-3" />
+                  <a href={`#/Kambaz/Courses/${cid}/Assignments/${assignment._id}`} className="wd-assignment-link">
+                    View Assignment
+                  </a>
                 </div>
-              <LessonControlButtons />
-            </ListGroup.Item>
-
-            <ListGroup.Item className="wd-assignment p-3 ps-1 d-flex justify-content-between align-items-center">
-              <div>
-                <BsGripVertical className="me-2 fs-3" />
-                <a href="#/Kambaz/Courses/1234/Assignments/123"
-               className="wd-assignment-link" >
-              A2 - CSS + BOOTSTRAP
-            </a> 
-                <div> Multiple Modules | <b>Not available until</b> May 13 at 12:00am | </div>
-                <div> <b>Due</b> May 20 at 11:59pm | 100 pts </div>
-              </div>
-              <LessonControlButtons />
-            </ListGroup.Item>
-
-            <ListGroup.Item className="wd-assignment p-3 ps-1 d-flex justify-content-between align-items-center">
-              <div>
-                <BsGripVertical className="me-2 fs-3" />
-                <a href="#/Kambaz/Courses/1234/Assignments/123"
-               className="wd-assignment-link" >
-              A3 - JAVASCRIPT + REACT
-            </a> 
-                <div> Multiple Modules | <b>Not available until</b> May 20 at 12:00am | </div>
-                <div> <b>Due</b> May 26 at 11:59pm | 100 pts </div>
-              </div>
-              <LessonControlButtons />
-            </ListGroup.Item>
-          </ListGroup>
-        </ListGroup.Item>
+                <LessonControlButtons />
+              </ListGroup.Item>
+            </ListGroup>
+          </ListGroup.Item>
+        ))}
       </ListGroup>
     </div>
   );
--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -30,69 +30,6 @@ export default function Modules() {
                     <BsGripVertical className="me-2 fs-3" /> {lesson.name} <LessonControlButtons />
                   </ListGroup.Item>
                 ))}</ListGroup>)}</ListGroup.Item>))}
-        {/* <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary text-white d-flex justify-content-between align-items-center">
-            <div>
-              <BsGripVertical className="me-2 fs-3" />
-              Week 1
-            </div>
-            <ModuleControlButtons />
-          </div>
-
-          <ListGroup className="wd-lessons rounded-0">
-            <ListGroup.Item className="wd-lesson p-3 ps-1 d-flex justify-content-between align-items-center">
-              <div>
-                <BsGripVertical className="me-2 fs-3" />
-                LEARNING OBJECTIVES
-              </div>
-              <LessonControlButtons />
-            </ListGroup.Item>
-
-            <ListGroup.Item className="wd-lesson p-3 ps-1 d-flex justify-content-between align-items-center">
-              <div>
-                <BsGripVertical className="me-2 fs-3" />
-                Introduction to the course
-              </div>
-              <LessonControlButtons />
-            </ListGroup.Item>
-
-            <ListGroup.Item className="wd-lesson p-3 ps-1 d-flex justify-content-between align-items-center">
-              <div>
-                <BsGripVertical className="me-2 fs-3" />
-                Learn what is Web Development
-              </div>
-              <LessonControlButtons />
-            </ListGroup.Item>
-          </ListGroup>
-        </ListGroup.Item>
-
-        <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary text-white d-flex justify-content-between align-items-center">
-            <div>
-              <BsGripVertical className="me-2 fs-3" />
-              Week 2
-            </div>
-            <ModuleControlButtons />
-          </div>
-
-          <ListGroup className="wd-lessons rounded-0">
-            <ListGroup.Item className="wd-lesson p-3 ps-1 d-flex justify-content-between align-items-center">
-              <div>
-                <BsGripVertical className="me-2 fs-3" />
-                LESSON 1
-              </div>
-              <LessonControlButtons />
-            </ListGroup.Item>
-
-            <ListGroup.Item className="wd-lesson p-3 ps-1 d-flex justify-content-between align-items-center">
-              <div>
-                <BsGripVertical className="me-2 fs-3" />
-                LESSON 2
-              </div>
-              <LessonControlButtons />
-            </ListGroup.Item>
-          </ListGroup>
-        </ListGroup.Item> */}
       </ListGroup>
     </div>
   );
