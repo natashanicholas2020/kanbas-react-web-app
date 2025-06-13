521d132fe4c1ba64bdd43448b262d938b84b1d45
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-13 17:45:05 -0400

    finished 4.4.3.3

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -7,6 +7,7 @@ import { BsGripVertical } from "react-icons/bs";
 import { useParams } from "react-router";
 import * as db from "../../Database";
 import { v4 as uuidv4 } from "uuid";
+import { FormControl } from "react-bootstrap";
 
 export default function Modules() {
   const { cid } = useParams();
@@ -24,6 +25,14 @@ export default function Modules() {
     setModules(modules.filter((m) => m._id !== moduleId));
   };
 
+  const editModule = (moduleId: string) => {
+    setModules(modules.map((m) => (m._id === moduleId ? { ...m, editing: true } : m)));
+  };
+
+  const updateModule = (module: any) => {
+    setModules(modules.map((m) => (m._id === module._id ? module : m)));
+  };
+
   return (
     <div>
       <ModulesControls setModuleName={setModuleName} moduleName={moduleName} addModule={addModule}/><br /><br /><br />
@@ -33,8 +42,22 @@ export default function Modules() {
           .map((module: any) => (
           <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
             <div className="wd-title p-3 ps-2 bg-secondary">
-              <BsGripVertical className="me-2 fs-3" /> {module.name} <ModuleControlButtons moduleId={module._id}
-        deleteModule={deleteModule}/>
+            <BsGripVertical className="me-2 fs-3" />
+            {!module.editing && module.name}
+            { module.editing && (
+              <FormControl className="w-50 d-inline-block"
+                    onChange={(e) => updateModule({ ...module, name: e.target.value })}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        updateModule({ ...module, editing: false });
+                      }
+                    }}
+                    defaultValue={module.name}/>
+            )}
+                <ModuleControlButtons
+                moduleId={module._id}
+                deleteModule={deleteModule}
+                editModule={editModule}/>
             </div>
             {module.lessons && (
               <ListGroup className="wd-lessons rounded-0">
