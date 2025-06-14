db5807a7e5b599e7564a4381c20d49fb96f7084c
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-14 11:53:47 -0400

    will now be adding the reducer for assignments

--- src/Kambaz/Courses/Assignments/Editor.tsx
+++ src/Kambaz/Courses/Assignments/Editor.tsx
@@ -102,4 +102,3 @@ export default function AssignmentEditor() {
     </div>
   );
 }
-
--- src/Kambaz/Courses/Modules/reducer.ts
+++ src/Kambaz/Courses/Modules/reducer.ts
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from "uuid";
 const initialState = {
   modules: modules,
 };
+
 const modulesSlice = createSlice({
   name: "modules",
   initialState,
@@ -18,15 +19,18 @@ const modulesSlice = createSlice({
       };
       state.modules = [...state.modules, newModule] as any;
     },
+
     deleteModule: (state, { payload: moduleId }) => {
       state.modules = state.modules.filter(
         (m: any) => m._id !== moduleId);
     },
+
     updateModule: (state, { payload: module }) => {
       state.modules = state.modules.map((m: any) =>
         m._id === module._id ? module : m
       ) as any;
     },
+    
     editModule: (state, { payload: moduleId }) => {
       state.modules = state.modules.map((m: any) =>
         m._id === moduleId ? { ...m, editing: true } : m
