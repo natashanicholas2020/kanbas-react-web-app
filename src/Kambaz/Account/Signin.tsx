9a11284242fc764b20d841f784151405e2d48192
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-16 11:48:23 -0400

    finished 5.3.2.1

--- src/Kambaz/Account/Signin.tsx
+++ src/Kambaz/Account/Signin.tsx
@@ -1,41 +1,25 @@
-// import { Link, } from "react-router-dom";
-// import { Form } from "react-bootstrap";
-
-// export default function Signin() {
-//   return (
-//     <div id="wd-signin-screen">
-//       <h1>Sign in</h1>
-//       <Form.Control id="wd-username"
-//              placeholder="username"
-//              className="mb-2"/>
-//       <Form.Control id="wd-password"
-//              placeholder="password" type="password"
-//              className="mb-2"/>
-//       <Link id="wd-signin-btn"
-//             to="/Kambaz/Account/Profile"
-//             className="btn btn-primary w-100 mb-2">
-//             Sign in </Link>
-//       <Link id="wd-signup-link" to="/Kambaz/Account/Signup">Sign up</Link>
-//     </div> );}
-
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { setCurrentUser } from "./reducer";
 import { useDispatch } from "react-redux";
 import * as db from "../Database";
 import { FormControl, Button } from "react-bootstrap";
+import * as client from "./client";
 
 export default function Signin() {
   const [credentials, setCredentials] = useState<any>({});
+  
   const dispatch = useDispatch();
+
   const navigate = useNavigate();
-  const signin = () => {
-    const user = db.users.find(
-      (u: any) => u.username === credentials.username && u.password === credentials.password);
+
+  const signin = async () => {
+    const user =  await client.signin(credentials);
     if (!user) return;
     dispatch(setCurrentUser(user));
     navigate("/Kambaz/Dashboard");
   };
+
   return (
     <div id="wd-signin-screen">
       <h1>Sign in</h1>
