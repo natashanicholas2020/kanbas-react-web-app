de7bafa21e85fcd91c5dfcfd58829d03729a47e0
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-16 22:25:05 -0400

    finished 6.2.6.4

--- src/Kambaz/Courses/People/Table.tsx
+++ src/Kambaz/Courses/People/Table.tsx
@@ -1,11 +1,15 @@
 import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
 import { users, enrollments } from "../../Database";
+import PeopleDetails from "./Details";
+import { Link } from "react-router-dom";
+
 
 export default function PeopleTable({ users = [] }: { users?: any[] }) {
 
   return (
     <div id="wd-people-table">
+      <PeopleDetails />
       <Table striped>
         <thead>
           <tr>
@@ -21,9 +25,11 @@ export default function PeopleTable({ users = [] }: { users?: any[] }) {
           {users.map((user: any) => (
             <tr key={user._id}>
               <td className="wd-full-name text-nowrap">
+              <Link to={`/Kambaz/Account/Users/${user._id}`} className="text-decoration-none">
                 <FaUserCircle className="me-2 fs-1 text-secondary" />
                 <span className="wd-first-name">{user.firstName}</span>{" "}
                 <span className="wd-last-name">{user.lastName}</span>
+                </Link>
               </td>
               <td className="wd-login-id">{user.loginId}</td>
               <td className="wd-section">{user.section}</td>
