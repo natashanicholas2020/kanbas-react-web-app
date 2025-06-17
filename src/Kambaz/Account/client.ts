e94577ad9718e647d7e72dd388f8ff73bc6191ad
Natasha Nicholas <nnicholas2020@gmail.com>
2025-06-16 20:22:27 -0400

    finished 6.2.6.3

--- src/Kambaz/Account/client.ts
+++ src/Kambaz/Account/client.ts
@@ -43,3 +43,15 @@ export const signout = async () => {
     return response.data;
   };
   
+export const findUsersByRole = async (role: string) => {
+    const response = await
+      axios.get(`${USERS_API}?role=${role}`);
+    return response.data;
+  };
+
+export const findUsersByPartialName = async (name: string) => {
+    const response = await axios.get(`${USERS_API}?name=${name}`);
+    return response.data;
+  };
+  
+  
